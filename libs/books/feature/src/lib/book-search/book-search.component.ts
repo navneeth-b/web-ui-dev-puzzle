fd6a390a264e27414c211f3138a7e10a452cbfcb
Navneeth Battula <navneethkrishna.battula@concentrix.com>
2023-05-19 17:50:28 +0530

    Instant search changes

--- libs/books/feature/src/lib/book-search/book-search.component.ts
+++ libs/books/feature/src/lib/book-search/book-search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
 import {
   addToReadingList,
@@ -9,14 +9,16 @@ import {
 } from '@tmo/books/data-access';
 import { FormBuilder } from '@angular/forms';
 import { Book } from '@tmo/shared/models';
-import { Observable } from 'rxjs';
+import { fromEvent, Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
 @Component({
   selector: 'tmo-book-search',
   templateUrl: './book-search.component.html',
   styleUrls: ['./book-search.component.scss']
 })
-export class BookSearchComponent {
+export class BookSearchComponent implements OnInit {
+  @ViewChild('searchInput', { static: true }) searchInput: ElementRef;
   books$: Observable<ReadingListBook[]> = this.store.select(getAllBooks);
 
   searchForm = this.fb.group({
@@ -30,6 +32,14 @@ export class BookSearchComponent {
 
   get searchTerm(): string {
     return this.searchForm.value.term;
+  }
+
+  ngOnInit(): void {
+    fromEvent(this.searchInput.nativeElement, 'keyup')
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe(() => {
+        this.searchBooks();
+      });
   }
 
   formatDate(date: void | string) {
